ba2bb766b53c55f8a01527416d4e83bb81fd1d01
dwiayuwulandari <47851859+dwiayuwulandari@users.noreply.github.com>
2020-01-13 15:14:58 +0700

    Admin

--- application/views/Laporan.php
+++ application/views/Laporan.php
@@ -100,7 +100,7 @@
                 <ul class=" navbar-right">
                   <li class="nav-item dropdown open" style="padding-left: 15px;">
                     <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                      <img src="images/img.jpg" alt="">Imam Basori
+                      <img src="images/img.jpg" alt="">Admin
                     </a>
                     <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
                       

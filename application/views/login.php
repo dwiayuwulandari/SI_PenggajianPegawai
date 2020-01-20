dd91d9821f7539425c7099091b85b60d1eb6877d
RagaSatyaAirlangga <47999119+RagaSatyaAirlangga@users.noreply.github.com>
2020-01-20 07:51:02 +0700

    tampilan login
    view

--- application/views/login.php
+++ application/views/login.php
@@ -1,103 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<title>CV.Karya Agung Jaya</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="<?php echo base_url();?>assets/login/images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/css/util.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/css/main.css">
-<!--===============================================================================================-->
-</head>
-<body class="bg-default">
-  <div class="main-content">
-          <!-- Collapse header -->
-              <div class="col-/6 collapse-close">
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
-                  <span></span>
-                  <span></span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <div class="container mt--8 pb-5">
-      <div class="row justify-content-center">
-        <div class="col-lg-5 col-md-7">
-          <div class="card bg-secondary shadow border-0">
-            <div class="card-body px-lg-5 py-lg-5">
-              <?= $this->session->flashdata('message'); ?>
-              <form role="form" method="post" action="<?php echo site_url('Login/index'); ?>">
-                <div class="Login mb-5" style="margin-left: 33%;">
-                  <h1 class="text-white">LOGIN</h1>
+    <title>CV. Karya Agung Jaya </title>
+
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url();?>assets/sigap//vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="<?php echo base_url();?>assets/sigap//vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="<?php echo base_url();?>assets/sigap//vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="<?php echo base_url();?>assets/sigap//vendors/animate.css/animate.min.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="<?php echo base_url();?>assets/sigap//build/css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="login">
+    <div>
+      <a class="hiddenanchor" id="signup"></a>
+      <a class="hiddenanchor" id="signin"></a>
+
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content">
+            <?= $this->session->flashdata('message'); ?>
+            <form role="form" method="post" action="<?php echo site_url('Login/index'); ?>">
+              <h1>Login Form</h1>
+              <div>
+                <input class="form-control" placeholder="Username" type="username" id="username" name="username" />
+              </div>
+              <?php echo form_error('username', '<small class="text-danger-pl-3">', '</small>'); ?>
+              <div>
+                <input class="form-control" placeholder="Password" type="password" id="password" name="password"" />
+              </div>
+              <?php echo form_error('password', '<small class="text-danger-pl-3">', '</small>'); ?>
+              <div  class="text-center">
+                <button type="submit" class="btn btn-lg btn-success btn-block" >Login</button>
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+                <p class="change_link">Mau Masuk ?
+                  <a class="to_register">Silahkan Login Terlebih Dahulu </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1></i>CV. Karya Agung Jaya</h1>
+                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
                 </div>
-                <div class="form-group mb-3">
-                  <div class="input-group input-group-alternative">
-                    <div class="input-gr oup-prepend">
-                      <span class="input-group-text"><i class="ni ni-circle-08"></i></span>
-                    </div>
-                    <input class="form-control" placeholder="Username" type="username" id="username" name="username">
-                  </div>
-                  <?php echo form_error('username', '<small class="text-danger-pl-3">', '</small>'); ?>
-                </div>
-                <div class="form-group">
-                  <div class="input-group input-group-alternative">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
-                    </div>
-                    <input class="form-control" placeholder="Password" type="password" id="password" name="password">
-                  </div>
-                  <?php echo form_error('password', '<small class="text-danger-pl-3">', '</small>'); ?>
-                </div>
-                <div class="text-center">
-                  <button type="submit" class="btn btn-lg btn-success btn-block">L O G I N</button>
-                </div>
-              </form>
-            </div>
-          </div>
+              </div>
+            </form role="form" method="post" action="<?php echo site_url('Login/index'); ?>">
+          </section>
         </div>
+
+
       </div>
     </div>
-  </div>
-
-	
-<!--===============================================================================================-->
-	<script src="<?php echo base_url();?>assets/login/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="<?php echo base_url();?>assets/login/vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-	<script src="<?php echo base_url();?>assets/login/vendor/bootstrap/js/popper.js"></script>
-	<script src="<?php echo base_url();?>assets/login/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="<?php echo base_url();?>assets/login/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="<?php echo base_url();?>assets/login/vendor/daterangepicker/moment.min.js"></script>
-	<script src="<?php echo base_url();?>assets/login/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="<?php echo base_url();?>assets/login/vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-	<script src="<?php echo base_url();?>assets/login/js/main.js"></script>
-
-</body>
+  </body>
 </html>
